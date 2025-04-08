0d517f28521bf0dc05e589e78dfb65852b96c8af
turtleman1278 <schen7812@gmail.com>
2025-04-08 17:52:30 -0400

    database password change

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "zyxqir-Bytgud-2jybwe",
+  password: "",
   database: "kitchen_cabinet",
 });
 
